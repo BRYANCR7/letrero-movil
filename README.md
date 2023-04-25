d082bab0d49077b0f130e7199b11eac0a15d843c
BRYANCR7 <131827160+BRYANCR7@users.noreply.github.com>
2023-04-25 17:59:13 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Abra esta pagina en [https://bryancr7.github.io/letrero-movil/](https://bryancr7.github.io/letrero-movil/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/bryancr7/letrero-movil** e importar
+
+## Edita este proyecto ![Insignia de estado de compilación](https://github.com/bryancr7/letrero-movil/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/bryancr7/letrero-movil** y haga clic en importar
+
+## Vista previa de bloques
+
+Esta imagen muestra el código de los bloques de la última confirmación en master.
+Esta imagen puede tardar unos minutos en actualizarse.
+
+![Una vista renderizada de los bloques](https://github.com/bryancr7/letrero-movil/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,84 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . # # # .
+                # # # # #
+    `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . # # # .
+                . # # # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . . # # .
+                . . # # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . # .
+                . . . # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . #
+    `)
+    basic.pause(100)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . #
+    `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . # .
+                . . . # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . . # # .
+                . . # # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . # # # .
+                . # # # #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . # # # .
+                # # # # #
+    `)
+    basic.pause(100)
+})
